f1cc0b5d5b6e76d374adb9aa0788f8bf3e257d94
Arman85 <advanced315@gmail.com>
2019-03-16 12:56:06 +0600

    lesson 6 finished

--- public/index.php
+++ public/index.php
@@ -2,9 +2,14 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once  LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 
 new \ishop\App();
 
-\ishop\App::$app->setProperty('test', 'TEST');
+//\ishop\App::$app->setProperty('test', 'TEST');
+//
+//debug( \ishop\App::$app->getProperties() );
 
-debug( \ishop\App::$app->getProperties() );
+//throw new Exception ( 'Страница не найдена', 500 );
+
+//debug( \ishop\Router::getRoutes() );
