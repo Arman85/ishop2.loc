e6eb0bcc3d0b6cd495d703efe2439385a316a4e1
Arman85 <advanced315@gmail.com>
2019-03-18 16:11:07 +0600

    lesson 7 finished

---
+++ app/controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 18.03.2019
+ * Time: 16:02
+ */
+
+namespace app\controllers;
+
+
+class MainController
+{
+
+    public function indexAction()
+    {
+        echo __METHOD__ ;
+    }
+
+}
---
+++ app/controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 18.03.2019
+ * Time: 16:07
+ */
+
+namespace app\controllers;
+
+
+class PageController
+{
+
+    public function viewAction()
+    {
+        echo __METHOD__;
+    }
+
+}
